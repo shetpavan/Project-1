6d66e180c5349f8d69a1c5998421060912dab9d4
Pavan Shet <112113930+shetpavan@users.noreply.github.com>
2024-02-17 13:53:34 +0530

    Add files via upload

---
+++ Task-1/Face_Recognition/face_recognition.py
@@ -0,0 +1,34 @@
+#pylint:disable=no-member
+
+import numpy as np
+import cv2 as cv
+
+haar_cascade = cv.CascadeClassifier('Face_Detection\haar_face.xml')
+
+people = ['Fumio Kishida', 'Joe Biden', 'Narendra Modi']
+# features = np.load('features.npy', allow_pickle=True)
+# labels = np.load('labels.npy')
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+face_recognizer.read(r'C:\Users\test\Desktop\Task-1\Face_Recognition\face_trained.yml')
+
+img = cv.imread(r'C:\Users\test\Desktop\Task-1\Face_Recognition\Test Images\FumioKishida116.jpg')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Person', gray)
+
+# Detect the face in the image
+faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+for (x,y,w,h) in faces_rect:
+    faces_roi = gray[y:y+h,x:x+w]
+
+    label, confidence = face_recognizer.predict(faces_roi)
+    print(f'Label = {people[label]} with a confidence of {confidence}')
+
+    cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Face', img)
+
+cv.waitKey(0)
