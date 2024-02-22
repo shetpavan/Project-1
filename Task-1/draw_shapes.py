1bd79f36f25730dac2ff87fd22adff30882df8f4
Pavan Shet <112113930+shetpavan@users.noreply.github.com>
2024-02-22 12:16:55 +0530

    Create draw_shapes.py

---
+++ Task-1/draw_shapes.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500,500,3), dtype='uint8')
+cv.imshow('Blank', blank)
+
+blank[200:300, 300:400] = 0,0,255
+cv.imshow('Green', blank)
+
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
+cv.imshow('Rectangle', blank)
+
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=-1)
+cv.imshow('Circle', blank)
+
+cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
+cv.imshow('Line', blank)
+
+cv.putText(blank, 'Hello, my name is Pavan!!!', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
+cv.imshow('Text', blank)
+
+cv.waitKey(0)
